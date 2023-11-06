480ba4292f8faa2c5ccc02ae7c6e91aa8197b6c0
benjamin <benkisenge03@gmail.com>
2023-11-06 12:23:27 +0200

    Add required to the field name

--- custom_addons/school/models/department.py
+++ custom_addons/school/models/department.py
@@ -4,4 +4,4 @@ class Department(models.Model):
     _name = 'school_table.department' 
     _description = "School department Model"
 
-    name = fields.Char(string="Name", required=True) 
+    name = fields.Char(string="Name", required=True)
